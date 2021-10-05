be8a6f64d214401becb9ab715200c77a104b6259
Max <lavor.maxim@gmail.com>
2021-10-05 12:44:54 +0300

    Board Fabric, Make Turn Service, Bug Fix, add Pawn representation, new TurnCheck VErsion

---
+++ Queridor/BoardFabricAbstract/IBoardFabric.cs
@@ -0,0 +1,14 @@
+﻿using Queridor.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queridor.BoardFabricAbstract
+{
+    public interface IBoardFabric
+    {
+        Board CreateBoard();
+
+        Pawn CreatePawn(Board board, bool isFirstPlayer);
+    }
+}
--- Queridor/Model/Corner.cs
+++ Queridor/Model/Corner.cs
@@ -6,8 +6,6 @@ namespace Queridor.Model
 {
     public class Corner
     {
-        public int X { get; set; }
-        public int Y { get; set; }
         public KeyValuePair<Edge, Edge> HorizontalEdges { get; set; }
         public KeyValuePair<Edge, Edge> VerticalEdges { get; set; }
     }
