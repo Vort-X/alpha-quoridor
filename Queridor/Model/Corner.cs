11cc92484834cecc6b982e8a2e8ed6980032e5d8
Max <lavor.maxim@gmail.com>
2021-10-07 17:23:31 +0300

    Added coordinates to corners

--- Queridor/Model/Corner.cs
+++ Queridor/Model/Corner.cs
@@ -6,6 +6,8 @@ namespace Queridor.Model
 {
     public class Corner
     {
+        public int X { get; set; }
+        public int Y { get; set; }
         public KeyValuePair<Edge, Edge> HorizontalEdges { get; set; }
         public KeyValuePair<Edge, Edge> VerticalEdges { get; set; }
     }
