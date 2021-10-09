bd480ea6d0b09d2c0333e270ec8197f34ad4a28a
Max <lavor.maxim@gmail.com>
2021-10-09 22:23:59 +0300

    new version of setWallCheck

--- Queridor/Services/TurnCheckService.cs
+++ Queridor/Services/TurnCheckService.cs
@@ -67,11 +67,34 @@ namespace Queridor.Services
 
         public bool CanPlaceWallCheck(List<Cell> cells, Corner corner, bool horizontal, Pawn player, Pawn enemy)
         {
-            if (!CheckIfWinPathExist(player, FindWinCells(player, cells))
-                || !CheckIfWinPathExist(player, FindWinCells(enemy, cells)))
+            if ((horizontal & (corner.HorizontalEdges.Key.IsBlocked || corner.HorizontalEdges.Key.IsBlocked))
+                || (!horizontal & (corner.VerticalEdges.Key.IsBlocked || corner.VerticalEdges.Key.IsBlocked)))
                 return false;
-            return horizontal ? (!corner.HorizontalEdges.Key.IsBlocked & !corner.HorizontalEdges.Key.IsBlocked) 
-                : (!corner.VerticalEdges.Key.IsBlocked & !corner.VerticalEdges.Key.IsBlocked);
+
+            MakeTurnService.PlaceWall(corner, horizontal);
+            if (!CheckIfWinPathExist(player, FindWinCells(player, cells))
+                || !CheckIfWinPathExist(enemy, FindWinCells(enemy, cells)))
+            {
+                DestroyWalls(corner, horizontal);
+                return false;
+            }
+
+            DestroyWalls(corner, horizontal);
+            return true;
+        }
+
+        private void DestroyWalls(Corner corner, bool horizontal)
+        {
+            if (horizontal)
+            {
+                corner.HorizontalEdges.Key.IsBlocked = false;
+                corner.HorizontalEdges.Value.IsBlocked = false;
+            }
+            else
+            {
+                corner.VerticalEdges.Key.IsBlocked = false;
+                corner.VerticalEdges.Value.IsBlocked = false;
+            }
         }
 
         private bool CheckIfWinPathExist(Pawn player, List<Cell> winCells)
