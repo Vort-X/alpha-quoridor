439e4f72a8459e92522f7ddbc2ad4165bf392758
Mihailo <mihailpro02@gmail.com>
2021-10-09 15:18:04 +0300

    easy bot implementation

--- Quoridor.Model/PlayerTypes/EasyBotPlayer.cs
+++ Quoridor.Model/PlayerTypes/EasyBotPlayer.cs
@@ -1,6 +1,7 @@
 ﻿using Queridor.Model;
 using Queridor.ServicesAbstract;
 using Quoridor.Model.Abstract;
+using Quoridor.Model.TurnFactories;
 using Quoridor.Model.Turns;
 using System;
 using System.Collections.Generic;
@@ -12,11 +13,13 @@ namespace Quoridor.Model.PlayerTypes
 {
     class EasyBotPlayer : BotPlayer
     {
+        private readonly Board board;
         private readonly IBoardPresenter boardPresenter;
         private readonly ITurnCheckService turnCheckService;
 
-        public EasyBotPlayer(Pawn pawn, IBoardPresenter boardPresenter, ITurnCheckService turnCheckService) : base(pawn)
+        public EasyBotPlayer(Pawn pawn, Board board, IBoardPresenter boardPresenter, ITurnCheckService turnCheckService) : base(pawn)
         {
+            this.board = board;
             this.boardPresenter = boardPresenter;
             this.turnCheckService = turnCheckService;
         }
@@ -29,11 +32,18 @@ namespace Quoridor.Model.PlayerTypes
             {
                 var ns = turnCheckService.FindAvaliableNeighbours(pawn.Cell);
                 var r = random.Next(0, ns.Count);
-                return null;
+                return MakeMoveTurnFactory.CreateTurn(pawn, ns[r].X, ns[r].Y);
             }
             else
             {
-                throw new NotImplementedException();
+                bool horizontal = true;
+                if (random.NextDouble() < 0.5)
+                {
+                    horizontal = false;
+                }
+                var cs = board.Corners.Where(c => turnCheckService.CanPlaceWallCheck(board.Cells, c, horizontal, pawn, enemy)).ToList();
+                var r = random.Next(0, cs.Count);
+                return PlaceWallTurnFactory.CreateTurn(pawn, cs[r].X, cs[r].Y, horizontal);
             }
         }
     }
